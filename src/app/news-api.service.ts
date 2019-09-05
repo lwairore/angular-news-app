f3d4468a41cdec0e18dc0232cbac11d31efee3fa
lwairore <kwairore@gmail.com>
2019-09-05 20:56:30 +0300

    Import HttpClient from '@angular/common/http'. Set api_key.

--- src/app/news-api.service.ts
+++ src/app/news-api.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
+import {  HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NewsApiService {
+  api_key = '85fec7291c8048028f8c53b5678725f0'
 
   constructor() { }
 }
