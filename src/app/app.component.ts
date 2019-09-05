7f241a2a0bca356b9c297ee4fdfec1dfa14e1390
lwairore <kwairore@gmail.com>
2019-09-05 21:06:42 +0300

    Import NewsApiService from 'news-api.service'. Define a constructor inside AppComponent class. Inject NewsApiService inside the constructor. Alias NewsApiService as newsapi. Set newsapi as private.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NewsApiService } from './news-api.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'news-app';
+  
+  
+  constructor(private newsapi:NewsApiService){
+    console.log('app component constructor called.')
+  }
+  
 }
