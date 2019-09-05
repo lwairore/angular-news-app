2f3bd33cc4e5ddc773b3ee55fd063600c8918a01
lwairore <kwairore@gmail.com>
2019-09-05 20:54:19 +0300

    Fix NewsApiService from './news-api-service' to './news-api.service'.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule, MatCardModule, MatMenuModule, MatToolbarModule, MatIconModule, MatSidenavModule, MatListModule } from '@angular/material';
 
-import { NewsApiService } from './news-api-service';
+import { NewsApiService } from './news-api.service';
 import { HttpClientModule } from '@angular/common/http';
 
 
